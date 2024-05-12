a2731d524933d6b5975e01390a034fecf8f375ef
“anjana <74060929+anjana-nanayakkara@users.noreply.github.com>
2024-05-12 23:55:06 +0530

    added routings

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Header from "./components/Header";
 
 const App = () => {
   return (
-    <div className='text-3xl text-re'>App</div>
-  )
-}
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About/>}></Route>
+        <Route path="/sign-in" element={<SignIn></SignIn>}></Route>
+        <Route path="/sign-up" element={<SignUp></SignUp>}></Route>
+        <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
+        <Route path="/projects" element={<Projects></Projects>}></Route>
+        {/* <Route path="/" element={<></>}></Route> */}
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
